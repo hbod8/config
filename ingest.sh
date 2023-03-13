c33702ca7a4510093a35f0e834a6e8354c37c4da
Harry Saliba <harrysaliba4139@gmail.com>
2023-03-12 21:44:13 -0700

    Update ingest.sh

--- ingest.sh
+++ ingest.sh
@@ -1,5 +1,17 @@
-FOLDER=$(date +%Y-%m-%d)
-mkdir -v ~/Pictures/imported/$FOLDER
-#cp -v $1/* ~/Pictures/imported/$FOLDER
-#mv -v $1/* ~/Pictures/imported/$FOLDER
-mv -v --backup=numbered $1/* ~/Pictures/imported/$FOLDER
+#!/bin/bash
+set -e
+
+FOLDER="/home/harry/Pictures/imported/$(date +%Y-%m-%d)"
+TEMP=$(mktemp -d)
+echo "Copying files from source to temporary directory ($1 => $TEMP)..."
+rsync --human-readable --info=progress2 --recursive $1/ $TEMP
+if test $FOLDER; then
+  echo "Destination folder exists ($FOLDER)..."
+else
+  echo "Creating destination folder ($FOLDER)..."
+  mkdir $FOLDER
+fi
+echo "Copying to destination folder ($TEMP => $FOLDER)..."
+rsync --human-readable --info=progress2 --recursive $TEMP/ $FOLDER
+echo "Removing source files ($1)..."
+rm -r $1/*
